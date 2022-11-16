252a3f97fbfb6c7466e48ce36811f331961e04cd
Ricardo Leon <ricardoleon_1@yahoo.com>
2022-11-16 16:13:29 -0600

    finished project

---
+++ src/Components/spinner.jsx
@@ -0,0 +1,17 @@
+import spinner from "../Images/spinner.gif";
+
+const Spinner = () => {
+  return (
+    <img
+      src={spinner}
+      alt="loading"
+      style={{
+        width: "200px",
+        margin: "auto",
+        display: "block",
+      }}
+    />
+  );
+};
+
+export default Spinner;
